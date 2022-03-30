788e732f1473a2db6dc5ae4f666b9461cdb4fb44
johntreppo <treppo.j@northeastern.edu>
2022-03-30 15:45:51 -0400

    printed data to console 2

--- js/visualization.js
+++ js/visualization.js
@@ -1,55 +1,51 @@
 //read in data
 d3.csv("data/crypto_data.csv").then((data) => {
-      console.log(data);
+    // set the dimensions and margins of the graph
+    const width = 500,
+        height = 500,
+        margin = 40;
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    const radius = Math.min(width, height) / 2 - margin;
+
+    // append the svg object to the div called 'my_dataviz'
+    const svg = d3.select("#pie-and-bump")
+      .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", `translate(${width/2}, ${height/2})`);
+
+    // set the color scale
+    const color = d3.scaleOrdinal()
+      .range(d3.schemeSet2)
+
+    // Compute the position of each group on the pie:
+    const pie = d3.pie()
+      .value(function(d) {return d[1]})
+    const data_ready = pie(Object.entries(data))
+
+    const arcGenerator = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg.selectAll('mySlices')
+      .data(data_ready)
+      .join('path')
+        .attr('d', arcGenerator)
+        .attr('fill', function(d){ return(color(d.data[1])) })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+
+    svg.selectAll('mySlices')
+      .data(data_ready)
+      .join('text')
+      .text(function(d){ return "grp " + d.data[0]})
+      .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
+      .style("text-anchor", "middle")
+      .style("font-size", 17)
 });
 
 
-// set the dimensions and margins of the graph
-const width = 450,
-    height = 450,
-    margin = 40;
-
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-const radius = Math.min(width, height) / 2 - margin;
-
-// append the svg object to the div called 'my_dataviz'
-const svg = d3.select("#pie-and-bump")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", `translate(${width/2}, ${height/2})`);
-
-// Create dummy data
-const data = {a: 9, b: 20, c:30, d:8, e:12}
-
-// set the color scale
-const color = d3.scaleOrdinal()
-  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
-
-// Compute the position of each group on the pie:
-const pie = d3.pie()
-  .value(function(d) {return d[1]})
-const data_ready = pie(Object.entries(data))
-
-const arcGenerator = d3.arc()
-  .innerRadius(0)
-  .outerRadius(radius)
-
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg.selectAll('mySlices')
-  .data(data_ready)
-  .join('path')
-    .attr('d', arcGenerator)
-    .attr('fill', function(d){ return(color(d.data[1])) })
-    .attr("stroke", "black")
-    .style("stroke-width", "2px")
-    .style("opacity", 0.7)
-
-svg.selectAll('mySlices')
-  .data(data_ready)
-  .join('text')
-  .text(function(d){ return "grp " + d.data[0]})
-  .attr("transform", function(d) { return `translate(${arcGenerator.centroid(d)})`})
-  .style("text-anchor", "middle")
-  .style("font-size", 17)
